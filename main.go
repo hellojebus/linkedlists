03be09d6520b7b27294cb98d9fa2a17f26739586
hellojebus <dev@hellojeb.us>
2018-10-14 20:34:26 -0700

    update add node method

--- main.go
+++ main.go
@@ -8,6 +8,23 @@ type Node struct {
 }
 
 func addNode(t *Node, v int) int {
+	if root == nil {
+		t = &Node{v, nil}
+		root = t
+		return 0
+	}
+
+	if v == t.Value {
+		fmt.Println("Node already exists:", v)
+		return -1
+	}
+
+	if t.Next == nil {
+		t.Next = &Node{v, nil}
+		return -2
+	}
+
+	return addNode(t.Next, v)
 }
 
 func main() {
